47c28b28060fa7234075d39393bd2aa8495543d8
Adi <adi4vista@gmail.com>
2021-10-18 10:17:19 +0700

    add solusi enhanced untuk kasus-1

---
+++ kasus_01b.py
@@ -0,0 +1,76 @@
+# soal:
+# Buatlah script program python yang dapat menerima input data berupa nim dan nama lengkap anda masing-masing. 
+# Lengkapi program anda dengan exception handling untuk menangani semua kemungkinan exception saat runtime yang bisa terjadi. 
+# Semua karakter pada nim harus berupa angka numeric. Timbulkan exception jika nim yang diinput user tidak memenuhi syarat.
+# Program kemudian dapat melakukan hal-hal berikut ini:
+# Menghitung banyak karakter pada data nim anda
+# Menghitung banyak karakter angka ganjil
+# Menghitung banyak karakter angka genap
+# Menjumlahkan angka-angka pada data nim anda
+# Menghitung banyak karakter huruf (a-z) pada data nama anda, spasi tidak ikut dihitung.
+
+# solusi:
+
+import os
+
+
+while True:
+    os.system('cls')
+    try:
+        # input
+        nim = input('Nim  ? ')
+        nama = input('Nama ? ')
+
+        # validasi - nim harus berisi angka semua
+        if not nim.isnumeric():
+            raise Exception('Sorry, Nim Harus Berupa Angka Numeric Semua ... ')
+
+    except Exception as e:
+        print(f'Error: {e}')
+        
+        print()
+        while True:
+            ulang = input('ulangi kembali dari awal (y/n) ? ')
+            if ulang in ('y', 'n'):
+                break
+
+    else:
+        # Menghitung banyak karakter pada data nim anda
+        panjang_nim = len(nim)
+
+        # Menghitung banyak karakter angka ganjil
+        # Menghitung banyak karakter angka genap
+        # Menjumlahkan angka-angka pada data nim anda
+        hitung_ganjil = 0
+        hitung_genap = 0
+        jumlah = 0
+        for c in nim:
+            n = int(c)
+            if n % 2:
+                # ganjil
+                hitung_ganjil += 1
+            else:
+                hitung_genap += 1
+            jumlah += n
+
+        # Menghitung banyak karakter huruf (a-z) pada data nama anda, spasi tidak ikut dihitung
+        panjang_nama = len(nama.replace(' ', ''))
+
+        print('\nHasil Analisa:')
+        print('--------------')
+        print(f'Banyak karakter pada nim anda               = {panjang_nim:3} karakter')
+        print(f'Banyak karakter angka ganjil                = {hitung_ganjil:3} karakter')
+        print(f'Banyak karakter angka genap                 = {hitung_genap:3} karakter')
+        print(f'Jumlah dari keseluruhan angka pada nim anda = {jumlah:3}')
+        print(f'Banyak karakter huruf pada nama anda        = {panjang_nama:3} karakter')
+
+        print()
+        while True:
+            ulang = input('coba lagi (y/n) ? ')
+            if ulang in ('y', 'n'):
+                break
+    
+    if ulang == 'n':
+        os.system('cls')
+        break
+
